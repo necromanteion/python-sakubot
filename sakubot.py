f1b11f7b174e85d7b93a22a96338cd53caffb0fa
necromanteion <wmmedlar@gmail.com>
2016-03-05 23:55:38 -0600

    included cmd.py and padx_parser.py in bot commands

---
+++ sakubot.py
@@ -0,0 +1,69 @@
+import asyncio
+import random
+import os
+import chitchat as cc
+
+import cmds
+import padx_parser as pdx
+
+sakubot = cc.Client()
+
+# register plugins
+sakubot.on(nick='necromanteion', text=cc.firstword('.belly'))(cmds.belly)
+sakubot.on(nick=cmds.not_necro, text=cc.firstword('.belly'))(cmds.belly2)
+sakubot.on(text=cc.firstword('.paizuri'))(cmds.paizuri)
+sakubot.on(text=cc.firstword('.help'))(cmds.help)
+sakubot.on(text=cc.firstword('.compare'))(pdx.compare)
+
+@sakubot.on('connected')
+def identify(*args):
+    """Identify nick and user with the server, send server password, and join channels."""
+    yield cc.identify(nickname='Sakubot', username='v3')
+    
+    # wait for the welcome message to know we've successfully joined
+    yield from sakubot.wait_for(cc.constants.RPL_WELCOME)
+    
+    password = os.environ.get('RIZON_BOT')
+    yield cc.privmsg('nickserv', 'identify {0}'.format(password))
+    
+    # wait for our password to be accepted so our vhost will show up
+    yield from sakubot.wait_for('NOTICE', text='password accepted')
+    
+    yield cc.join('#sakubot', '#padg')
+
+
+@sakubot.on('ping')
+def pong(prefix, target):
+    return cc.pong(target)
+
+
+@sakubot.on('PRIVMSG', text=cc.firstword('.rem'))
+def rem(prefix, target, text):    
+    mons = 'Sakuya Meimei Leilan Haku Karin Artemis Freyja LKali DKali Salamanderbelly'.split()
+    line = 'You rolled a {0}, {1}-nii!'.format(random.choice(mons), prefix.nick)        
+    
+    return cc.privmsg(target, line)
+    
+    
+@sakubot.on(text='.quit', nick='necromanteion')
+def quit(*args):
+    yield cc.quit()
+    
+    # if you're using BaseEventLoop.run_forever, BaseEventLoop.stop should be called
+    # somewhere in a function called upon disconnecting from the server to stop release
+    # the blocking call
+    sakubot.loop.stop()
+
+
+@sakubot.on('kick', text='Sakubot')
+def rejoin(prefix, channel, target, reason):
+    return cc.join(channel)
+    
+
+@sakubot.on('DISCONNECTED')
+async def reconnect(host, port, exc):
+    await sakubot.connect(host=host, port=port)
+   
+    
+if __name__ == '__main__':
+    sakubot.run(host='irc.rizon.net', port=6667)
