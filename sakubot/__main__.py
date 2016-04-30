56664ef08be367af1672a4fbfad2506b091e80a1
necromanteion <wmmedlar@gmail.com>
2016-04-29 22:11:30 -0500

    SSL by default

--- sakubot/__main__.py
+++ sakubot/__main__.py
@@ -1,5 +1,6 @@
+import ssl
 import types
 from . import sakubot
 
 sakubot.vars = types.SimpleNamespace()
-sakubot.run(host='irc.rizon.net', port=6667)
+sakubot.run(host='irc.rizon.net', port=6697, ssl=ssl.get_default_context())
