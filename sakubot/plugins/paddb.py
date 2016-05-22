a8e13ef9a2ba889ddddad191747939447d800507
necromanteion <wmmedlar@gmail.com>
2016-05-22 16:34:32 -0500

    updated api for aditional lookups

--- sakubot/plugins/paddb.py
+++ sakubot/plugins/paddb.py
@@ -6,7 +6,8 @@ from sakubot import sakubot
 
 
 Card = collections.namedtuple('Card', ['id', 'name', 'rarity', 'cost', 'lv_max',
-                                       'feed_xp', 'released', 'sell_price', 'mp'])
+                                       'feed_xp', 'released', 'sell_price', 'mp',
+                                       'attrs'])
 
 PATH = os.path.join(os.path.dirname(__file__), 'pad.db')
 
@@ -29,12 +30,79 @@ def close(*args):
         pass
     
     
+def fetch_cursor(query, *params):
+    """
+    Convenience wrapper around `cursor.execute`.
+    
+    Equivalent to `connection.cursor().execute(query, params)`.
+    """
+    
+    cursor = sakubot.vars.dbconn.cursor()
+    return cursor.execute(query, params)
+
+
+def fetch_one(query, *params):
+    """
+    Fetch a single row from a query result.
+    
+    Equivalent to `connection.cursor().execute(query, params).fetchone()`.
+    """
+    cursor = fetch_cursor(query, *params)
+    
+    return cursor.fetchone()
+    
+    
+def fetch_all(query, *params):
+    """
+    Fetch all rows from a query result.
+    
+    Equivalent to `connection.cursor().execute(query, params).fetchall()`.
+    """
+    cursor = fetch_cursor(query, *params)
+    
+    return cursor.fetchall()
+    
+    
 def name(card_id):
     """Look up only a card's name by its id. Returns the str name or None."""
     
     card = lookup(card_id)
     
     return card.name if card else None
+
+
+def attributes(card_id):
+    """Look up a card's attributes by its id. Returns a list of str attributes."""
+    
+    query = '''
+            SELECT attr.name
+            FROM   card_attributes c
+                   JOIN attributes attr
+                     ON c.attribute = attr.id
+            WHERE  c.card = ?
+            ORDER  BY c.n;
+            '''
+    
+    attributes = [row[0] for row in fetch_all(query, card_id)]
+    
+    return attributes
+
+
+def awakenings(card_id):
+    """Look up a card's awakenings by its id. Returns a list of str awakenings."""
+    
+    query = '''
+            SELECT awk.name
+            FROM   card_awakenings c
+                   JOIN awakenings awk
+                     ON c.awakening = awk.id
+            WHERE  c.card = ?
+            ORDER  BY c.n;
+            '''
+    
+    awakenings = [row[0] for row in fetch_all(query, card_id)]
+    
+    return awakenings
     
 
 def lookup(card_id):
@@ -46,6 +114,6 @@ def lookup(card_id):
     data = cursor.execute(query, (card_id,)).fetchone()
     
     if data:
-        return Card(*data)
+        return Card(*data, attributes(card_id))
     
     return None
